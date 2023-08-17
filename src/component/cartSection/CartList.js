c3feb953acc42fb39333471ba30a82f4e1bfddbd
Pavan Rane <ranepavanrp@gmail.com>
2023-08-17 13:31:43 +0530

    update: updated the price of eachproduct with its quantity in cartlist

--- src/component/cartSection/CartList.js
+++ src/component/cartSection/CartList.js
@@ -2,6 +2,7 @@ import React from 'react';
 import CartQuantity from './CartQuantity';
 
 const CartList = ({cartItems,handleDecrement, handleIncrement, handleRemoveItem}) => {
+
   return (
     <div className="grid gap-4">
     {cartItems.map((item) => (
@@ -10,7 +11,7 @@ const CartList = ({cartItems,handleDecrement, handleIncrement, handleRemoveItem}
           <img src={item.product.image} alt={item.product.title} className="w-20 h-20 object-contain mr-4" />
           <div>
             <p className="text-base font-semibold mb-1">{item.product.title}</p>
-            <p className="text-gray-600">${item.product.price}</p>
+            <p className="text-gray-600">${+(item.product.price * item.quantity).toFixed(2)}</p>
             <div className="mt-2 space-x-2">
               <button
                 type='button'
