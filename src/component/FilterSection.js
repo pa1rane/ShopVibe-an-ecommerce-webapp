4e64fd12df897c5056548b3aad5a0a26ebeaaaed
Pavan Rane <ranepavanrp@gmail.com>
2023-08-23 14:24:07 +0530

    Add: added filter by price Filter type into filtersection component to filter the products by price range

--- src/component/FilterSection.js
+++ src/component/FilterSection.js
@@ -5,7 +5,8 @@ import ProductList from './ProductList';
 const FilterSection = () => {
 
   const [state] = useContext(ShopContext);
-  const [input, setInput] = useState("");  
+  const [searchInput, setSearchInput] = useState("");
+  const [sliderValue, setSliderValue] = useState('1000');
   const [filteredProducts, setFilteredProducts] = useState([]);
 
   useEffect(() => {
@@ -13,12 +14,12 @@ const FilterSection = () => {
   }, [state.list])
 
   const handleChange = (e) => {
-    setInput(e.target.value);
+    setSearchInput(e.target.value);
     filterProducts(e.target.value)
   }
 
   const handleCategoryChange =(category)=> {
-    setInput("");
+    setSearchInput("");
     if(category === 'All') {
         setFilteredProducts(state.list);
     } else {
@@ -34,18 +35,32 @@ const FilterSection = () => {
     setFilteredProducts(filterList);
   };
 
+  const handlePriceRange = (e) => {
+     const filterPriceRange = state.list.filter((li) => 
+      li.price <= e.target.value
+     );
+     setSliderValue(e.target.value);
+     setFilteredProducts(filterPriceRange);
+  }
+
   return (
     <>
     <div className='flex'>
     <div className="flex flex-col">
-      <input type="text" placeholder='Search' value={input} onChange={handleChange}/>
+      <input type="text" placeholder='Search' value={searchInput} onChange={handleChange}/>
       <h3>Category</h3>
       <p onClick={() => handleCategoryChange("All")} className='hover:cursor-pointer'>All</p>
       <p onClick={() => handleCategoryChange("men's clothing")} className='hover:cursor-pointer'>Mens</p>
       <p onClick={() => handleCategoryChange("women's clothing")} className='hover:cursor-pointer'>Womens</p>
       <p onClick={() => handleCategoryChange("jewelery")} className='hover:cursor-pointer'>Jewellery</p>
       <p onClick={() => handleCategoryChange("electronics")} className='hover:cursor-pointer'>Electronics</p>
-      
+      <h3>Filter By Price</h3>
+      <input type="range" 
+      min = "10"
+      max= "1000"
+      value={sliderValue}
+      onChange={handlePriceRange}
+      />
       </div>
       <ProductList products={filteredProducts}/>
       </div>
