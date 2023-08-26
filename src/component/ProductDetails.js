5d86f5dbde79266e9f7d770ca7337dda526a6e19
Pavan Rane <ranepavanrp@gmail.com>
2023-08-26 16:13:20 +0530

    add: added styling to the productDetails

--- src/component/ProductDetails.js
+++ src/component/ProductDetails.js
@@ -1,54 +1,75 @@
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
 import GridLoader from "react-spinners/GridLoader";
 
-
 const ProductDetails = () => {
- 
-  const {id} = useParams();
+  const { id } = useParams();
   const [product, setProduct] = useState(null);
   const [loading, setLoading] = useState(true);
-  
-  useEffect(() =>  {
-    const apiUrl = `https://fakestoreapi.com/products/${id}`
+
+  useEffect(() => {
+    const apiUrl = `https://fakestoreapi.com/products/${id}`;
 
     const fetchProduct = async () => {
       try {
         const response = await axios.get(apiUrl);
         setProduct(response.data);
-        setLoading(false)
+        setLoading(false);
       } catch (error) {
-        console.log(error)
+        console.log(error);
         setLoading(false);
       }
     };
-    
-    fetchProduct()
+
+    fetchProduct();
   }, [id]);
 
   if (loading) {
-    return  (
-    <GridLoader
-    color="black"
-    loading={loading}
-    size={150}
-    aria-label="Loading Spinner"
-    data-testid="loader"
-  />
-    )
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <GridLoader
+          color="black"
+          loading={loading}
+          size={150}
+          aria-label="Loading Spinner"
+          data-testid="loader"
+        />
+      </div>
+    );
   }
 
   return (
-    <>
-      <div>
-        <img src={product.image} alt={product.title} />
-        <h3>{product.title}</h3>
-        <p>{product.price}$</p>
-        <p>{product.description}</p>
+    <div className="flex items-center justify-center min-h-screen bg-[#EADBC8]">
+      <div className="max-w-screen-xl mx-auto px-4 py-8 gap-60 bg-white border rounded-lg shadow-md md:flex">
+        <div>
+          <img
+            src={product.image}
+            alt={product.title}
+            className="w-30 h-80 ml-10"
+          />
+        </div>
+        <div className="md:w-1/2 md:pl-8 mt-0 flex flex-col justify-center items-center">
+          <h3 className="text-xl font-semibold mb-2 text-center">
+            {product.title}
+          </h3>
+          <p className="text-gray-600 mb-4">${product.price}</p>
+          <p className="text-gray-800 mb-4 text-center">
+            {product.description}
+          </p>
+          <button
+            className="bg-blue-500 
+          text-white px-4 py-2 rounded-lg
+           hover:bg-blue-900 
+           focus:outline-none 
+           focus:ring focus:ring-blue-200"
+          >
+            Add to Cart
+          </button>
+        </div>
       </div>
-    </>
-  )
-}
+    </div>
+  );
+};
 
-export default ProductDetails
+export default ProductDetails;
--- src/component/homepage/ProductSlider.js
+++ src/component/homepage/ProductSlider.js
@@ -1,20 +1,20 @@
 import React, {useContext} from 'react';
 import { ShopContext } from '../../App';
+import {motion} from 'framer-motion';
 
 const ProductSlider = () => {
 
   const [state,] = useContext(ShopContext);
   const sliderList = state.list.filter((li) => li.category === "women's clothing");
-  
  
   return (
-    <div className='flex'>
+    <div >
      {sliderList.map((product) => (
       <div key={product.id}>
-        <img src={product.image} alt={product.title} />
-      </div>
+        <img src={product.image} alt={product.title}/>
+        </div>
      ) )}
-    </div>
+     </div>
   )
 }
 
