28792da4c5e8d894359aee747f19a4917b2cbd5e
Pavan Rane <ranepavanrp@gmail.com>
2023-08-29 10:45:28 +0530

    Added testimonials data

---
+++ src/testimonials data.js
@@ -0,0 +1,50 @@
+ const data = { 
+    "testimonials": [
+      {
+        "id": 1,
+        "name": "Michelle K.",
+        "rating": 5,
+        "review": "I'm absolutely in love with ShopVibe! The quality of their clothing is outstanding. I recently bought a stunning evening dress for a special occasion, and I received so many compliments. The fit was perfect, and the fabric felt luxurious. I can't wait to shop here again!",
+        "image": "https://images.unsplash.com/photo-1541216970279-affbfdd55aa8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80"
+      },
+      {
+        "id": 2,
+        "name": "Jason M.",
+        "rating": 4,
+        "review": "As a fashion-conscious guy, I'm always on the lookout for trendy clothing. ShopVibe never disappoints! I got a sleek leather jacket that not only looks awesome but also keeps me warm. The shipping was super fast, and the customer service was top-notch. I'm giving it four stars because there's always room for improvement.",
+        "image": "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8bWVufGVufDB8fDB8fHww&auto=format&fit=crop&w=1000&q=60"
+      },
+      {
+        "id": 3,
+        "name": "Emily P.",
+        "rating": 5,
+        "review": "ShopVibe is my go-to online store for all things fashion. The range of options they offer is incredible – from casual wear to formal attire. I recently ordered a cute summer dress, and it arrived sooner than I expected. The dress fit perfectly, and the quality was beyond my expectations. Five stars all the way!",
+        "image": "https://images.unsplash.com/photo-1635488640163-e5f6782cda6e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fGdpcmx8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=1000&q=60"
+      },
+      {
+        "id": 4,
+        "name": "Alex S.",
+        "rating": 5,
+        "review": "I've shopped at various online clothing stores, but ShopVibe stands out. The user-friendly website made browsing a breeze, and the detailed size guide ensured I got the right fit. I bought a pair of jeans that fit like a glove and a graphic tee that's now my favorite. Excellent job, ShopVibe!",
+        "image": "https://images.unsplash.com/photo-1508341591423-4347099e1f19?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8bWVufGVufDB8fDB8fHww&auto=format&fit=crop&w=1000&q=60"
+      },
+      {
+        "id": 5,
+        "name": "Sophia L.",
+        "rating": 4,
+        "review": "ShopVibe has a wonderful collection, but what I appreciate the most is their commitment to sustainability. I got an organic cotton hoodie, and I'm in love with it. Knowing that I'm supporting eco-friendly practices makes the shopping experience even better. I would give five stars if the checkout process were a bit smoother.",
+        "image": "https://images.unsplash.com/photo-1602233158242-3ba0ac4d2167?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8Z2lybHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=1000&q=60"
+      },
+      {
+        "id": 6,
+        "name": "Michael B.",
+        "rating": 5,
+        "review": "I'm a busy professional, and shopping isn't really my thing. However, ShopVibe changed that. Their curated collections make it easy for me to pick stylish",
+        "image": "https://images.unsplash.com/photo-1610088441520-4352457e7095?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fG1lbnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=1000&q=60"
+      }
+    ]
+  }
+
+  export default data;
+
+  
