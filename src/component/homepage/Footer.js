395d1aae0caaf9b6722174915f70e8ed63f63502
Pavan Rane <ranepavanrp@gmail.com>
2023-08-31 15:32:47 +0530

    style: gave styling to the footer component

--- src/component/homepage/Footer.js
+++ src/component/homepage/Footer.js
@@ -2,27 +2,33 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <div>
+    <>
+        <div className='flex justify-between p-8'>
       <div className='companyInfo'>
-       <h3>COMPANY INFO</h3>
-        <p>About Us</p>
-        <p>FAQs</p>
-        <p>Terms & Conditions</p>
-        <p>Privacy Policy</p>
-        <p>Shipping Policy</p>
+       <h3 className='font-bold mb-2'>COMPANY INFO</h3>
+        <p className='cursor-pointer font-serif hover:underline'>About Us</p>
+        <p className='cursor-pointer font-serif hover:underline'>FAQs</p>
+        <p className='cursor-pointer font-serif hover:underline'>Terms & Conditions</p>
+        <p className='cursor-pointer font-serif hover:underline'>Privacy Policy</p>
+        <p className='cursor-pointer font-serif hover:underline'>Shipping Policy</p>
       </div>
       <div className='orderInfo'>
-       <h3>ORDER INFO</h3>
-        <p>Track Order</p>
-        <p>Create Return/Exchange</p>
-        <p>Shipping Policy</p>
-        <p>Refund Policy</p>
+       <h3 className='font-bold mb-2'>ORDER INFO</h3>
+        <p className='cursor-pointer font-serif hover:underline'>Track Order</p>
+        <p className='cursor-pointer font-serif hover:underline'>Create Return/Exchange</p>
+        <p className='cursor-pointer font-serif hover:underline'>Shipping Policy</p>
+        <p className='cursor-pointer font-serif hover:underline'>Refund Policy</p>
       </div>
       <div className='companyInfo'>
-       <h3>CUSTOMER SERVICE</h3>
-        <p>Contact Us</p>
+       <h3 className='font-bold mb-2'>CUSTOMER SERVICE</h3>
+        <p className='cursor-pointer font-serif hover:underline'>Contact Us</p>
+        <p className='cursor-pointer font-serif hover:underline'>support@shopvibe.in</p>
       </div>
-    </div>
+      </div>
+      <footer className='flex justify-center items-center bg-[#4E148C] p-4 text-[#ffff]' >
+        © ShopVibe 2023
+      </footer>
+    </>
   )
 }
 
