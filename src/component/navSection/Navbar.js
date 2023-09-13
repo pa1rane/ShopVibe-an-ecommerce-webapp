ceec75be7c17a1e917cd14c0a16249e30a42938d
Pavan Rane <ranepavanrp@gmail.com>
2023-09-13 16:51:44 +0530

    feat: Display cart and wishlist lengths in Navbar

--- src/component/navSection/Navbar.js
+++ src/component/navSection/Navbar.js
@@ -1,101 +1,79 @@
-// import React from 'react';
-// import {BsFillBookmarkHeartFill} from 'react-icons/bs';
-// import {BsCartCheckFill} from 'react-icons/bs';
-// import { Link } from 'react-router-dom';
-// import UserProfile from './UserProfile';
-
-// const Navbar = () => {
-//   return (
-//     <div className='w-full  min-h-[10vh] flex items-center justify-between bg-[#4E148C] py-4 sticky top-0 z-10' >
-//       <div className="flex gap-20">
-//       <h3 className='ml-10 font-bold font-mono hover:cursor-pointer text-[#ffff] text-2xl' >ShopVibe</h3>
-//       <ul className='flex gap-10 text-[#ffff] items-center'>
-//      <Link to="/">
-//      <li className='hover:cursor-pointer hover:text-[#20a2b6] font-semibold transition-colors'>Home</li>
-//       </Link>
-//       <Link to="/products">
-//       <li className='hover:cursor-pointer hover:text-[#20a2b6] font-semibold transition-colors'>Products</li>
-//       </Link>
-//       <Link to="/about">
-//        <li className='hover:cursor-pointer hover:text-[#20a2b6] font-semibold transition-colors'>About</li>
-//        </Link>   
-//       </ul>
-//       </div>  
-  
-//       <div className="right">
-//       <div className="flex items-center mr-10 gap-10">
-//       <Link to="/wishlist">
-//         <BsFillBookmarkHeartFill className={`hover:cursor-pointer hover:text-[#20a2b6] font-semibold transition-colors w-6 h-5 text-[#ffff]`}/>
-//         </Link> 
-//      <Link to="/cart">
-//       <BsCartCheckFill className={`hover:cursor-pointer hover:text-[#20a2b6] font-semibold transition-colors w-6 h-6 text-[#ffff]`}/>
-//       </Link>
-//       <UserProfile className="hover:cursor-pointer w-6 h-6 text-[#ffff]"/>
-//       </div>   
-//       </div>
-//       </div>
-//   )
-// }
-// export default Navbar;
-
-
-import React, { useState } from 'react';
-import {BsFillBookmarkHeartFill} from 'react-icons/bs';
-import {BsCartCheckFill} from 'react-icons/bs';
-import {GiHamburgerMenu} from 'react-icons/gi';
-import { NavLink } from 'react-router-dom';
-import UserProfile from './UserProfile';
-import styles from './navbar.module.css';
+import React, { useContext, useEffect, useState } from "react";
+import { BsFillBookmarkHeartFill } from "react-icons/bs";
+import { BsCartCheckFill } from "react-icons/bs";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { NavLink } from "react-router-dom";
+import UserProfile from "./UserProfile";
+import styles from "./navbar.module.css";
+import { ShopContext } from "../../App";
 
 const Navbar = () => {
-
-  const [isMenuOpen, setIsMenuOpen] = useState(false)
+  const [state] = useContext(ShopContext);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [cartLength, setCartLength] = useState(0);
+  const [wishlistLength, setWishlistLength] = useState(0);
 
   const handleMenu = () => {
-    setIsMenuOpen(!isMenuOpen)
-  }
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  useEffect(() => {
+    setCartLength(state.cart.length);
+    setWishlistLength(state.wishlist.length);
+  }, [state]);
 
   return (
-    <div className={styles.navbar} >
-      <h3 className={styles.logo} >ShopVibe</h3>
+    <div className={styles.navbar}>
+      <h3 className={styles.logo}>ShopVibe</h3>
       <div className={styles.menuIcon}>
-      <GiHamburgerMenu onClick={handleMenu}/>
+        <GiHamburgerMenu onClick={handleMenu} />
       </div>
       <nav className={isMenuOpen ? styles.menuOpen : styles.menuClose}>
-      <ul className={styles.leftNavList}>
-     <NavLink
-      to="/"
-      className={({isActive}) => (isActive ? styles.active : "")}>
-      Home
-      </NavLink>
-      <NavLink 
-      to="/products"
-      className={({isActive}) => (isActive ? styles.active : "")}>
-      Products
-      </NavLink>
-      <NavLink 
-      to="/about"
-      className={({isActive}) => (isActive ? styles.active : "")}>
-       About
-       </NavLink>   
-      </ul>
-      <div className={styles.rightNavList}>
-      <NavLink 
-      to="/wishlist"
-      className={({isActive}) => (isActive ? styles.active : "")}>
-        <BsFillBookmarkHeartFill className={styles.wishlist}/>
-        </NavLink> 
-     <NavLink 
-     to="/cart"
-     className={({isActive}) => (isActive ? styles.active : "")}>
-      <BsCartCheckFill className={styles.cart}/>
-      </NavLink>
-      <UserProfile className={styles.userLog}/>
-      </div>
+        <ul className={styles.leftNavList}>
+          <NavLink
+            to="/"
+            className={({ isActive }) => (isActive ? styles.active : "")}
+          >
+            Home
+          </NavLink>
+          <NavLink
+            to="/products"
+            className={({ isActive }) => (isActive ? styles.active : "")}
+          >
+            Products
+          </NavLink>
+          <NavLink
+            to="/about"
+            className={({ isActive }) => (isActive ? styles.active : "")}
+          >
+            About
+          </NavLink>
+        </ul>
+        <div className={styles.rightNavList}>
+          <div>
+            {wishlistLength > 0 && (
+              <p className={styles.wishlistLength}>{wishlistLength}</p>
+            )}
+            <NavLink
+              to="/wishlist"
+              className={({ isActive }) => (isActive ? styles.active : "")}
+            >
+              <BsFillBookmarkHeartFill className={styles.wishlist} />
+            </NavLink>
+          </div>
+          <div>
+            {cartLength > 0 && <p className={styles.cartLength}>{cartLength}</p>}
+            <NavLink
+              to="/cart"
+              className={({ isActive }) => (isActive ? styles.active : "")}
+            >
+              <BsCartCheckFill className={styles.cart} />
+            </NavLink>
+          </div>
+          <UserProfile className={styles.userLog} />
+        </div>
       </nav>
-      </div>   
-      
-
-  )
-}
+    </div>
+  );
+};
 export default Navbar;
